6a501e13a5f2de661de53c69b9246a3e2e3c4c18
shs <orolsyeo@gmail.com>
2022-04-25 20:48:49 +0900

    generic test

--- src/com/sseob/Main.java
+++ src/com/sseob/Main.java
@@ -2,6 +2,20 @@ package com.sseob;
 
 public class Main {
 	public static void main(String[] args) {
+
+		Main main = new Main();
+		main.callWildCardMethod();
 		
+	}
+	
+	public void callWildCardMethod() {
+		WildcardGeneric<String> wildcard = new WildcardGeneric<String>();
+		wildcard.setWildcard("A");
+		wildcardStringMethod(wildcard);
+	}
+
+	private void wildcardStringMethod(WildcardGeneric<String> wildcard) {
+		String value = wildcard.getWildcard();
+		System.out.println(value);
 	}
 }
