ea85c32a8040b30c8aeefcb7043e6d391fad97cf
shs <orolsyeo@gmail.com>
2022-04-25 20:43:03 +0900

    initial commit

---
+++ src/com/sseob/Main.java
@@ -0,0 +1,7 @@
+package com.sseob;
+
+public class Main {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ src/com/sseob/WildcardGeneric.java
@@ -0,0 +1,14 @@
+package com.sseob;
+
+public class WildcardGeneric<W> {
+
+	W wildcard;
+	
+	public void setWildcard(W wildcard) {
+		this.wildcard = wildcard;
+	}
+	
+	public W getWildcard() {
+		return this.wildcard;
+	}
+}
