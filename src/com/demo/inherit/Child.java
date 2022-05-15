93984b3f150e599276b57b8150e09acdde86e543
hyun-sseob <orolsyeo@gmail.com>
2022-05-15 18:34:38 +0900

    inherit parent private method

--- src/com/demo/inherit/Child.java
+++ src/com/demo/inherit/Child.java
@@ -2,8 +2,9 @@ package com.demo.inherit;
 
 public class Child extends Parent{
 
-	public String printPrivate() {
+	// Parent type에 선언되어있는 private mehtod는 재정의 할 수 없다.
+	@Override
+	public void printPrivate() {
 		System.out.println("printPrivate() - child");
-		return "printPrivate() - child";
 	}
 }
